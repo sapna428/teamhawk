520acc75d1ccd0a5b94c50c00d0b771af0acb712
sapna428 <145866333+sapna428@users.noreply.github.com>
2023-11-20 23:19:36 +0500

    Update contactme.php

--- contactme.php
+++ contactme.php
@@ -1,21 +1,42 @@
-<?php   
-    require("./mailing/mailfunction.php");
 
-    $name = $_POST["name"];
-    $phone = $_POST['phone'];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
+<?php  
+if( isset($_POST['submit']) ) {
+//getting user data
+$name = $_POST['name'];
 
-    $body = "<ul><li>Name: ".$name."</li><li>Phone: ".$phone."</li><li>Email: ".$email."</li><li>Message: ".$message."</li></ul>";
+$email = $_POST['email'];
+$phone = $_POST['phone'];
 
-    $status = mailfunction("", "Company", $body); //reciever
-    if($status)
-        echo '<center><h1>Thanks! We will contact you soon.</h1></center>';
-    else
-        echo '<center><h1>Error sending message! Please try again.</h1></center>';    
+$message = $_POST['message'];
+ 
+//Recipient email, Replace with your email Address
+$mailTo = 'sapnakousar428.3@gmail.com';
+ 
+//email subject
+$subject = ' A New Message Received From ' .$name;
+ 
+//email message body
+$htmlContent = '<h2> Email Request Received </h2>
+<p> <b>Client Name: </b> '.$name  . '</p>
+<p> <b>Email: </b> '.$email .'</p>
+<p> <b>Phone Number: </b> '.$phone .'</p>
+
+<p> <b>Message: </b> '.$message .'</p>';
+ 
+//header for sender info
+$headers = "From: " .$name . "<". $email . ">";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+ 
+//PHP mailer function
+ $result = mail($mailTo, $subject, $htmlContent, $headers);
+ 
+   //error checking
+   if($result) {
+    $success = "The message was sent successfully!";
+   } else {
+    $failed = "Error: Message was not sent, Try again Later";
+   }
+}
+ 
 ?>
-
-<!-- ajxq jsln vblq bvpn -->
-
-
-
